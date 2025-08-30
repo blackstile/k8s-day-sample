6deaf55ba2887e949a8d10e24d7d9a7da98f9d36
William Miranda <blackstile@hotmail.com>
2025-08-30 12:28:43 -0300

    fix: port

--- app-llm/app.py
+++ app-llm/app.py
@@ -261,4 +261,4 @@ def chat():
 
 if __name__ == "__main__":
     logger.info("Iniciando a aplicação Flask com arquitetura de agentes ADK.")
-    app.run(host="0.0.0.0", port=9000)
+    app.run(host="0.0.0.0", port=5000)
